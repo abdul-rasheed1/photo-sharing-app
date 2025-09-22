bd6e349edddd0c024bbb91c8ae79112f0d4d9cf4
rasheed <rasheedjawula18@gmail.com>
2025-09-22 13:44:46 +0000

    built the sign up, login and logout routes for users

---
+++ src/routes/user-route.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
+import {signUp,findUser} from '../services/user_service.js';
+import jwt from 'jsonwebtoken'
+import 'dotenv/config';
+
+
+export const router = express.Router();
+
+router.post('/signup', async(req,res)=>{
+	try{
+	const {username, password, email} = req.body;
+	if (!username || !password || !email){
+		res.status(400).json({error: "Username, Password and email are required"});
+	}
+	if(password.length < 6 ){
+		res.status(400).json({error:"Password must be at least 6 characters long"});
+	}
+	if(!validator.isEmail(email)){
+		res.status(400).json({error:'provide a valid email address'});
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const signedUp = await signUp(username, hashedPassword, email);
+
+	return res.status(201).json({message:`user created successfully`, userid: signedUp.rows[0].id});
+
+	}
+catch(e){
+	console.error(e);
+	return res.status(500).json({error:"Internal Server Error"});
+}
+
+})
+
+
+router.post('/login', async(req,res)=>{
+	try{
+		const{username, password} = req.body;
+		const user = await findUser(username);
+		if(user.length === 0){
+			console.log(username or email not found)
+			return res.status(400).json({message: 'Invalid request data'});
+		}
+		const passwordCheck = await bcrypt.compare(password, user[0].password);
+
+		if(!passwordCheck){
+			console.log('Invalid credentials');
+			return res.status(400).json({message:'Invalid Credentials'});
+		}
+
+		console.log('Login successfull');
+		const token = jwt.sign({id:user[0].id}, process.env.JWT_SECRETE,{expiresIn:'5m'});
+
+		return res.status(200).json({message:'Login successfull', jwt:token});
+
+
+
+	}catch(e){
+		console.error(e);
+		return res.status(500).json({message:'Internal Server Error'});
+	}
+});
+
+
+
+router.post('/logout', async(req,res)=>{
+	res.status(200).json({message:'logged out succesfully'})
+})
+
+
+
+
