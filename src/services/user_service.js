f519f855694fc428fa1124473a37de87455000a4
rasheed <rasheedjawula18@gmail.com>
2025-10-04 18:49:16 +0000

    added functions to enable token refresh

--- src/services/user_service.js
+++ src/services/user_service.js
@@ -14,3 +14,23 @@ export const findUser = async(cred) =>{
 	console.log(result.rows);
 	return result.rows;
 } 
+
+export const sendToken = async(user_id, token)=>{
+	const query = 'INSERT INTO refresh_tokens (user_id, token) VALUES ($1, $2)';
+	const result = await pool.query(query,[user_id,token]);
+	console.log(result.rowCount);
+	return result.rowCount;
+}
+
+export const tokenCheck = async(user_id,token)=>{
+	const query = 'SELECT * FROM refresh_tokens WHERE user_id= $1 AND token = $2';
+	const result = await pool.query(query,[user_id, token]);
+	return result.rows;
+}
+
+
+export const deleteToken = async(user_id,token)=>{
+	const query = 'DELETE FROM refresh_tokens WHERE user_id= $1 AND token = $2';
+	const result = await pool.query(query,[user_id, token]);
+	return result.rowCount;
+}
