e3dc391443a48a7cdcdc05642ec0298571da6a4d
rasheed <rasheedjawula18@gmail.com>
2025-10-12 07:05:10 +0000

    new routes to get all posts, post from the user and delete posts

--- src/routes/post-route.js
+++ src/routes/post-route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {fileToCloud, urlToDb, postToDb, getAllPosts} from '../services/post-service.js';
+import {fileToCloud, urlToDb, postToDb, getAllPosts,delPost, userPosts} from '../services/post-service.js';
 import {upload} from '../middleware/multerMiddleware.js';
 import {jwtCheck} from '../middleware/authMiddleware.js';
 
@@ -33,7 +33,12 @@ postRouter.post('/createPost', jwtCheck,upload.single('photo'), async(req,res)=>
 		return res.status(500).json({message:"Internal Server Error"});
 	}
 
-}) 
+}) ;
+
+
+
+
+
 
 postRouter.get('/', jwtCheck, async(req,res)=>{
 	try{
@@ -45,4 +50,56 @@ postRouter.get('/', jwtCheck, async(req,res)=>{
 		console.error(e);
 		return res.status(500).json({message:"Internal Server Error"});
 	}
-})
+});
+
+
+
+
+
+
+
+postRouter.delete('/deletePost/:id', jwtCheck, async(req,res)=>{
+	try{
+		const {id:post_id} = req.params;
+		const {id:userId} = req.user;
+		const del = await delPost(post_id,userId);
+
+		if(del.rowCount === 0){
+			return res.status(404).json({message:"post not found"});
+		}
+
+		return res.status(204).json({message:"Delete Successfull", postId:del.rows[0].post_id});
+
+
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({message:"Internal Server Error"});
+	}
+});
+
+
+postRouter.get('/myposts', jwtCheck, async(req,res)=>{
+	try{
+	const userId = req.user.id;
+	const posts = await userPosts(userId);
+	return res.status(200).json({message:"posts retrieved", myPosts:posts});
+
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({message:"Internal Server Error"});
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
