adec6adfae73486acb011106c25b9f7773e688a4
rasheed <rasheedjawula18@gmail.com>
2025-10-12 07:23:23 +0000

    like and comment routes added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import {usersRouter} from './src/routes/user-route.js';
 import {postRouter} from './src/routes/post-route.js';
+import {likeRouter} from './src/routes/like-route.js';
+import {commentRouter} from './src/routes/comment-route.js';
+
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,8 @@ app.use(express.json());
 
 app.use('/users', usersRouter);
 app.use('/posts', postRouter);
+app.use('/like', likeRouter);
+app.use('/comment', commentRouter);
 
 
 
